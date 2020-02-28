be2ff3b48663f3820432024ef858560179f84b98
goldsnake0808 <goldsnake0808@gmail.com>
2020-02-28 17:24:38 +0900

    println2

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -50,6 +50,10 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         println("Menu")
+
+
+
+
         return true
     }
 
