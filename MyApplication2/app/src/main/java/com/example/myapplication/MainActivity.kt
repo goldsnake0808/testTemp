6a62fc56f695fff84686b2e94084b465bb018f9e
goldsnake0808 <goldsnake0808@gmail.com>
2020-04-21 16:58:16 +0900

    Revert "total"
    This reverts commit 0f7e2441d53aff37d322bec1af10de94caabcff7.

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
 
-        println("onCreate")
+        println("onCreate12")
         println("Menu")
 
         return true
