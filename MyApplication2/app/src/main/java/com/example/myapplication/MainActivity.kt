636a98dc0fa2bdd815eafe5fc1ee0b3843ec6e13
goldsnake0808 <goldsnake0808@gmail.com>
2020-04-21 16:09:34 +0900

    test12

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
-        println("onCreate")
+        println("onCreate12")
         return true
     }
 
